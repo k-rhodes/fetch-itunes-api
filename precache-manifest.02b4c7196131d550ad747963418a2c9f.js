c9034a717c9ebb756189df622c10d2e60ac8e788
Kyle <rhodestraveler@gmail.com>
2019-01-14 21:48:08 -0600

    Updates

---
+++ precache-manifest.02b4c7196131d550ad747963418a2c9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4054421e723af202ffe",
+    "url": "/fetch-itunes-api/static/js/runtime~main.d4054421.js"
+  },
+  {
+    "revision": "58c753ac81d70d7c70fd",
+    "url": "/fetch-itunes-api/static/js/main.58c753ac.chunk.js"
+  },
+  {
+    "revision": "f715b2e0bea123466b14",
+    "url": "/fetch-itunes-api/static/js/1.f715b2e0.chunk.js"
+  },
+  {
+    "revision": "58c753ac81d70d7c70fd",
+    "url": "/fetch-itunes-api/static/css/main.f9ebd4e3.chunk.css"
+  },
+  {
+    "revision": "655d7fc1150a019bde9c0d78a4e3f3cf",
+    "url": "/fetch-itunes-api/index.html"
+  }
+];
