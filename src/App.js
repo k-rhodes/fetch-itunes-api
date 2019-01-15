bce05b824ef16f62c340f1ba988c9d0f1852cc0b
Kyle <rhodestraveler@gmail.com>
2019-01-14 21:47:31 -0600

    installed gh-pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      artistData: {},
+      currentSearchQuery: ""
+    };
+
+    this.getSearchInput = this.getSearchInput.bind(this);
+    this.setSearchOutput = this.setSearchOutput.bind(this);
+  }
+
+  getSearchInput(event) {
+    const uriString = encodeURI(event.target.value);
+    this.state.currentSearchQuery = uriString;
+  }
+
+  setSearchOutput(event) {
+    event.preventDefault();
+
+    fetch(
+      `https://itunes.apple.com/search?term=${
+        this.state.currentSearchQuery
+      }&limit=50`
+    ).then(response => {
+      response.json().then(data => {
+        this.setState({
+          artistData: data.results
+        });
+      });
+    });
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+    let lis = [];
+
+    if (this.state.artistData.length >= 1) {
+      this.state.artistData.forEach(artist => {
+        lis.push(
+          <a href={artist.trackViewUrl} className="anc">
+            <li key={artist.trackId}>
+              {artist.trackName} By: {artist.artistName}
+            </li>
           </a>
+        );
+      });
+    }
+
+    return (
+      <div className="react-wrapper">
+        <header>
+          <h1>Search iTunes for your favorite artist</h1>
+          <form>
+            <input type="text" onChange={this.getSearchInput} />
+            <button onClick={this.setSearchOutput}>Search</button>
+          </form>
         </header>
+        <ul>{lis}</ul>
       </div>
     );
   }
